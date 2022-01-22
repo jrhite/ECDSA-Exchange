8f825824dfeddade2f0f1b1751d1719c2bebec95
Jacob Hite <1085644+jrhite@users.noreply.github.com>
2022-01-22 14:10:02 +0700

    challenge 1 & 2 solved

--- client/index.js
+++ client/index.js
@@ -1,6 +1,12 @@
+import sha256 from 'crypto-js/sha256';
+import { ec as EC } from 'elliptic';
 import "./index.scss";
 
+const ec = new EC('secp256k1');
+
 const server = "http://localhost:3042";
+
+let key;
 
 document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
   if(value === "") {
@@ -10,25 +16,62 @@ document.getElementById("exchange-address").addEventListener('input', ({ target:
 
   fetch(`${server}/balance/${value}`).then((response) => {
     return response.json();
-  }).then(({ balance }) => {
-    document.getElementById("balance").innerHTML = balance;
+  }).then(({ pk, balance }) => {
+    ////////////////////////////////////////
+    // CHALLENGE 2: BEGIN SKETCHY CODE
+    ////////////////////////////////////////
+
+    // WARNING! Extremely insecure code here, getting a private key returned
+    // by the server. For educational purposes only!
+    key = ec.keyFromPrivate(pk, 'hex');
+
+    document.getElementById('balance').innerHTML = balance;
+
+    ////////////////////////////////////////
+    // CHALLENGE 2: END SKETCHY CODE
+    ////////////////////////////////////////
   });
 });
 
 document.getElementById("transfer-amount").addEventListener('click', () => {
-  const sender = document.getElementById("exchange-address").value;
-  const amount = document.getElementById("send-amount").value;
-  const recipient = document.getElementById("recipient").value;
+  const sender = document.getElementById('exchange-address').value;
+  const amount = document.getElementById('send-amount').value;
+  const recipient = document.getElementById('recipient').value;
 
-  const body = JSON.stringify({
-    sender, amount, recipient
-  });
+  ////////////////////////////////////////
+  // CHALLENGE 2: BEGIN SIGN
+  ////////////////////////////////////////
+
+  const message = { sender, amount, recipient };
+  const messageString = JSON.stringify(message);
+
+  // convert message of arbitrary length to a fixed-length message hash digest
+  const messageDigest = sha256(messageString).toString();
+
+  // sign the message digest using the user's private key
+  const signature = key.sign(messageDigest).toDER('hex');
+
+  // send both the raw message and signature on the message to the server
+  // for verification
+  const body = JSON.stringify({ messageString, signature });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
 
-  fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
-    return response.json();
-  }).then(({ balance }) => {
-    document.getElementById("balance").innerHTML = balance;
-  });
+  fetch(request, { headers: { 'Content-Type': 'application/json' } })
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      if (typeof response.balance !== 'undefined') {
+        document.getElementById('balance').innerHTML = response.balance;
+      } else if (typeof response.error !== 'undefined') {
+        window.alert(response.error);
+      } else {
+        window.alert('Unexpected error occurred.');
+      }
+    });
+
+  ////////////////////////////////////////
+  // CHALLENGE 2: END SIGN
+  ////////////////////////////////////////
 });
--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+const sha256 = require('crypto-js/sha256');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -8,23 +12,89 @@ const port = 3042;
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+
+//////////////////////////
+// CHALLENGE 1: BEGIN
+//////////////////////////
+const accounts = [];
+const initialBalances = [100, 50, 75];
+const balances = {};
+
+console.log(`\nAvailable Accounts`);
+console.log(`==================`);
+
+for (let i = 0; i < initialBalances.length; i++) {
+  const key = ec.genKeyPair();
+  const privateKey = key.getPrivate('hex');
+  const publicKey = key.getPublic('hex');
+
+  accounts[publicKey] = privateKey;
+  balances[publicKey] = initialBalances[i];
+
+  console.log(`${i}:`);
+  console.log(`    Private Key: ${privateKey}`);
+  console.log(`    Public Key:  ${publicKey}`);
+  console.log(`    Balance:     ${balances[publicKey]}`);
 }
 
+console.log(`\n\n`);
+//////////////////////////
+// CHALLENGE 1: END
+//////////////////////////
+
 app.get('/balance/:address', (req, res) => {
-  const {address} = req.params;
+  const { address } = req.params;
+  console.log(`/balance/:address => address = ${address}`);
+
   const balance = balances[address] || 0;
-  res.send({ balance });
+
+  ////////////////////////////////////////
+  // CHALLENGE 2: BEGIN SKETCHY CODE
+  ////////////////////////////////////////
+
+  // WARNING! Extremely insecure code here, sending a private key from the
+  // server to the client. For educational purposes only!
+  const pk = accounts[address];
+
+  res.send({ pk, balance });
+  ////////////////////////////////////////
+  // CHALLENGE 2: END SKETCHY CODE
+  ////////////////////////////////////////
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+  const { messageString, signature } = req.body;
+  const message = JSON.parse(messageString);
+
+  const { sender, recipient, amount } = message;
+
+  ////////////////////////////////////////
+  // CHALLENGE 2: BEGIN VERIFY
+  ////////////////////////////////////////
+
+  // get signing/verifying key from public key
+  const key = ec.keyFromPublic(sender, 'hex');
+
+  // convert message of arbitrary length to a fixed-length message hash digest
+  const messageDigest = sha256(messageString).toString();
+
+  // verify the message digest against the signature using the sender's public key
+  const verified = key.verify(messageDigest, signature);
+
+  if (verified) {
+    balances[sender] -= amount;
+    balances[recipient] = (balances[recipient] || 0) + +amount;
+    res.send({ balance: balances[sender] });
+  } else {
+    res.send({
+      error:
+        'Digital signature does not match message. Have you been HACKED?!?!?',
+    });
+  }
+
+  ////////////////////////////////////////
+  // CHALLENGE 2: END VERIFY
+  ////////////////////////////////////////
 });
 
 app.listen(port, () => {
